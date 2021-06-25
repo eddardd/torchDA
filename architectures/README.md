f53e3a8d5add454a7a2601fb4642da5c89608018
Eduardo <edumontesuma@gmail.com>
2021-06-25 07:50:18 -0300

    updating available architectures

--- architectures/README.md
+++ architectures/README.md
@@ -2,4 +2,13 @@
 
 Available architectures:
 
-* LeNet-5, used in [Ganin et Al., 2016](https://jmlr.org/papers/volume17/15-239/15-239.pdf)
+* LeNet-5, proposed by [LeCun et al., 1998], used in [Ganin et Al., 2016](https://jmlr.org/papers/volume17/15-239/15-239.pdf) for digit adaptation (MNIST to MNIST-M and MNIST to SVHN).
+* SVHN, proposed by [Srivastava et al., 2014], used in [Ganin et Al., 2016](https://jmlr.org/papers/volume17/15-239/15-239.pdf)
+
+## References
+
+[LeCun et Al., 1998] LeCun, Y., Bottou, L., Bengio, Y., & Haffner, P. (1998). Gradient-based learning applied to document recognition. Proceedings of the IEEE, 86(11), 2278-2324.
+
+[Ganin et Al., 2016] Ganin, Y., Ustinova, E., Ajakan, H., Germain, P., Larochelle, H., Laviolette, F., ... & Lempitsky, V. (2016). Domain-adversarial training of neural networks. The journal of machine learning research, 17(1), 2096-2030.
+
+[Srivastava et Al., 2014] Srivastava, N., Hinton, G., Krizhevsky, A., Sutskever, I., & Salakhutdinov, R. (2014). Dropout: a simple way to prevent neural networks from overfitting. The journal of machine learning research, 15(1), 1929-1958.
--- architectures/svhn.py
+++ architectures/svhn.py
@@ -2,40 +2,44 @@ import torch
 from architectures.utils import LambdaLayer, GradReverse
 
 
-class LeNet5(torch.nn.Module):
+class SVHN(torch.nn.Module):
     def __init__(self, minimize_hdiv=False):
         self.minimize_hdiv = minimize_hdiv
-        super(LeNet5, self).__init__()
+        super(SVHN, self).__init__()
 
         self.feature_extractor = torch.nn.Sequential(
-            torch.nn.Conv2d(in_channels=1, out_channels=32, kernel_size=5),
+            torch.nn.Conv2d(in_channels=3, out_channels=64, kernel_size=5, padding=2),
             torch.nn.ReLU(),
             torch.nn.MaxPool2d(stride=2, kernel_size=2),
-            torch.nn.Conv2d(in_channels=32, out_channels=48, kernel_size=5),
-            torch.nn.ReLU()
+            torch.nn.Conv2d(in_channels=64, out_channels=64, kernel_size=5, padding=2),
+            torch.nn.ReLU(),
+            torch.nn.MaxPool2d(stride=2, kernel_size=2),
+            torch.nn.Conv2d(in_channels=64, out_channels=128, kernel_size=5, padding=2),
         )
 
         self.class_discriminator = torch.nn.Sequential(
-            torch.nn.Linear(in_features=48 * 8 * 8, out_features=100),
+            torch.nn.Linear(in_features=128 * 8 * 8, out_features=3072),
             torch.nn.ReLU(),
-            torch.nn.Linear(in_features=100, out_features=100),
+            torch.nn.Linear(in_features=3072, out_features=2048),
             torch.nn.ReLU(),
-            torch.nn.Linear(in_features=100, out_features=10),
+            torch.nn.Linear(in_features=2048, out_features=10),
             torch.nn.Softmax(dim=1)
         )
 
         if minimize_hdiv:
             self.domain_discriminator = torch.nn.Sequential(
                 LambdaLayer(GradReverse.apply),
-                torch.nn.Linear(in_features=48 * 8 * 8, out_features=100),
+                torch.nn.Linear(in_features=128 * 8 * 8, out_features=1024),
                 torch.nn.ReLU(),
-                torch.nn.Linear(in_features=100, out_features=1),
+                torch.nn.Linear(in_features=1024, out_features=1024),
+                torch.nn.ReLU(),
+                torch.nn.Linear(in_features=1024, out_features=1),
                 torch.nn.Sigmoid()
             )
 
     def forward(self, x):
         y = self.feature_extractor(x)
-        features = y.view(-1, 48 * 8 * 8)
+        features = y.view(-1, 128 * 8 * 8)
         predicted_labels = self.class_discriminator(features)
         
         if self.minimize_hdiv:
